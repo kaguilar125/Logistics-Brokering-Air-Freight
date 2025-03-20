117be44b10319c01866efc80390befd6c9c14a4c
kaguilar125 <113131954+kaguilar125@users.noreply.github.com>
2025-03-20 04:26:28 -0400

    last commit

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 $(document).ready(function () {
 
+
     window.scrollToSection = function (sectionId) {
         $('html, body').animate({
             scrollTop: $("#" + sectionId).offset().top
         }, 800);
     };
 
-    $(window).scroll(function () {
-        $(".img-fluid").each(function () {
-            let elementTop = $(this).offset().top;
-            let windowBottom = $(window).scrollTop() + $(window).height();
+ 
+    function isInViewport(element) {
+        let elementTop = $(element).offset().top;
+        let windowBottom = $(window).scrollTop() + $(window).height();
+        return elementTop < windowBottom - 100;
+    }
 
-            if (elementTop < windowBottom - 100) {
+ 
+    $(window).scroll(function () {
+        $(".img-fluid, .feedback-box").each(function () {
+            if (isInViewport(this) && $(this).css("opacity") == "0") {
                 $(this).animate({ opacity: 1 }, 500);
             }
         });
     });
 
-    $(".img-fluid").css("opacity", "0");
+
+    $(".img-fluid, .feedback-box").css("opacity", "0");
+
 });
